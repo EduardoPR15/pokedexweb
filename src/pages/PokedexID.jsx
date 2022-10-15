f2ef030abd0809e5973f0bec733c5ad95cf09525
EduardoPR15 <lpeduardo15@gmail.com>
2022-10-15 18:59:26 -0500

    v1

--- src/pages/PokedexID.jsx
+++ src/pages/PokedexID.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom'
 
 const PokedexID = () => {
+  const userStyle = useSelector(state => state.userStyle)
+  const trainerImgage = `/Trainers/${userStyle}.png`
+  const userName = useSelector(state => state.userName)
+
+const {id}  = useParams()
+
+const [pokemonID, setPokemonID] = useState()
+
+useEffect(() => {
+  const URL = `https://pokeapi.co/api/v2/pokemon/${id}/`
+  axios.get(URL)
+    .then(res => setPokemonID(res.data))
+    .catch(err => console.log(err))
+
+}, [])
+
+console.log(pokemonID)
   return (
-    <div>PokedexID</div>
+    <div>
+      <h2> nombre del pokemon</h2>
+      <div className='avatar'>
+        <div className="trainerStyleOn">
+        <img src={trainerImgage} alt="" />
+        
+        </div>
+        <div className="avatarName"> <p>{userName}</p></div>
+      </div>
+
+      <div className="IDprincipal">
+        <div className="IDeffect">
+          <div className="IDbg">
+            <div className="IDpokeName">
+              <h3>ID {id} {pokemonID?.name} </h3>
+              <div className="IDpokeIMG">
+                  <img src={pokemonID?.sprites.other["official-artwork"].front_default} alt="" />
+              </div>
+              <div className="IDpokeinfo">
+                <div className="IDpokeTypes">
+                {
+                        pokemonID?.types.map(type => (
+                          <li key={type.slot}>{type.type.name}</li>
+                        ))
+                      }
+                </div>
+                <div className="IDpokeStats">
+                {
+                        pokemonID?.stats.map(stat =>(
+                          <li key={stat.name}>
+                           <span>{stat.stat.name}</span> <span>{stat.base_stat}</span>
+                           </li>
+                        ))
+                      }
+                </div>
+                <div className="IDpokeGames">
+                  {
+                    pokemonID?.game_indices.map(game =>(
+                      <li> {game.version.name}</li>
+                    ))
+                  }
+                </div>
+                lo que sea necesario
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
