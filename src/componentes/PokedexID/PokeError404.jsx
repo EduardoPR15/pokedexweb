2657653f56d6d71cb5701ecc9f7b5944555c518d
EduardoPR15 <lpeduardo15@gmail.com>
2022-10-16 14:50:43 -0500

    errorpage

--- src/componentes/PokedexID/PokeError404.jsx
+++ src/componentes/PokedexID/PokeError404.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PokeError404 = () => {
   return (
+    <>
     <div>PokeError404\
         <div className="errorIMG">
             <img src="./error2.png" alt="" />
         </div>
+        <Link to ='/pokedex'><button>atras</button></Link>
+        
     </div>
+    </>
   )
 }
 
