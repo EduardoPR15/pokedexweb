7d959e35e9154b415f0efb14ba5e643ad6ace127
EduardoPR15 <lpeduardo15@gmail.com>
2022-10-18 16:05:08 -0500

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useNavigate } from 'react-router-dom';
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
@@ -7,10 +7,13 @@ import PokedexID from './pages/PokedexID'
 import ProtectedRoutes from './pages/ProtectedRoutes'
 
 function App() {
-
+  const navigate = useNavigate()
+const Return = () =>{
+  navigate('/pokedex')
+}
   return (
     <div className="App">      
-    <div className="pokeTitle"><img src="/Pokedex.png" alt="" /></div>
+    <div className="pokeTitle"><img onClick={Return} src="/Pokedex.png" alt="" /></div>
     <div className="container">
     <Routes>
       <Route path='/' element={<Home/>}/>
